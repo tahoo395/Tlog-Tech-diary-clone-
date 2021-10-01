b5f7bedb3ec8a1b14ff2e34dfa9980fdbb10d9fb
“Tahmid <tahmidahnafpro123@gmail.com>
2021-10-01 12:18:42 +0600

    init_final

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@
         relative
       "
     >
-      <p>Tlog</p>
+      <router-link to="/">Tlog</router-link>
       <div
         class="
           relative
@@ -109,7 +109,7 @@
         }"
       />
       <div class="md:hidden cursor-pointer" v-if="user">
-        <a href="http://localhost:8000/auth/logout">
+        <a href="https://za011b128-z699745d2-gtw.qovery.io/auth/logout">
           <svg
             class="text-gray-500 fill-current dark:text-gray-400 w-6 h-6"
             xmlns="http://www.w3.org/2000/svg"
@@ -132,7 +132,7 @@
           <p class="hidden md:block">{{ user.name }}</p>
         </div>
         <a
-          href="http://localhost:8000/auth/logout"
+          href="https://za011b128-z699745d2-gtw.qovery.io/auth/logout"
           class="
             logout
             hidden
@@ -159,7 +159,7 @@
         </a>
       </div>
       <div class="flex gap-5" v-else>
-        <a href="http://localhost:8000/auth/google"
+        <a href="https://za011b128-z699745d2-gtw.qovery.io/auth/google"
           ><svg
             width="20"
             role="img"
@@ -179,7 +179,7 @@
               d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
             ></path></svg
         ></a>
-        <a href="http://localhost:8000/auth/github"
+        <a href="https://za011b128-z699745d2-gtw.qovery.io/auth/github"
           ><svg
             width="20"
             role="img"
--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default() => {
     return axios.create({
-      baseURL: `http://localhost:8000`,
+      baseURL: `https://za011b128-z699745d2-gtw.qovery.io/`,
       withCredentials : true,
     })
   }
