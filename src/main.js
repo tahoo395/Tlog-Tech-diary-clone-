bd72ae2bc6275db3515f55e3d0e88f64e36c01a4
“Tahmid <tahmidahnafpro123@gmail.com>
2021-10-07 15:46:38 +0600

    frontend test for auth issue

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import router from "./router/index"
 import store from "./store.js"
 import App from './App.vue'
-import VueObserveVisibility from 'vue-observe-visibility'
 import './tailwind.scss'
 
-createApp(App).use(router).use(store).use(VueObserveVisibility).mount('#app')
+createApp(App).use(router).use(store).mount('#app')
